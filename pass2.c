1e9f295c8e2fe0c0a5b0aef2d281d05b4bf3bb8d
anson pinhero <ansonpinhero2016@outlook.com>
2018-11-15 15:47:05 +0000

    passtwo

---
+++ pass2.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{ 
+	char *x,*y,a[10],ad[10],label[10],opcode[10],operand[10],mnemonic[10],symbol[10];
+	int i,locctr,code,add,len,actual_len;
+	FILE *fp1,*fp2,*fp3,*fp4;
+	fp1=fopen("pass2out.txt","w");
+	fp2=fopen("symtab.txt","r");
+	fp3=fopen("intermediate.txt","r");
+	fp4=fopen("optab.txt","r");
+	fscanf(fp3,"%s%s%s",label,opcode,operand);
+	if(strcmp(opcode,"START")==0)
+	{
+		fprintf(fp1,"\t%s\t%s\t%s\n",label,opcode,operand);
+		fscanf(fp3,"%d%s%s%s",&locctr,label,opcode,operand);
+	}
+	while(strcmp(opcode,"END")!=0)
+	{ 
+		if(strcmp(opcode,"BYTE")==0)
+		{
+			fprintf(fp1,"%d\t%s\t%s\t%s\t",locctr,label,opcode,operand);
+			len=strlen(operand);
+			actual_len=len-3;
+			for(i=2;i<(actual_len+2);i++)
+			{ 
+				fprintf(fp1,"%x",operand[i]);
+			}
+			fprintf(fp1,"\n");
+		} 
+		else if(strcmp(opcode,"WORD")==0)
+		{ 
+			len=strlen(operand);
+	fprintf(fp1,"%d\t%s\t%s\t%s\t00000%d\n",locctr,label,opcode,operand,atoi(operand));
+		} 
+		else if((strcmp(opcode,"RESB")==0)||(strcmp(opcode,"RESW")==0))
+		{
+			fprintf(fp1,"%d\t%s\t%s\t%s\n",locctr,label,opcode,operand);
+		}
+		else
+		{
+			rewind(fp4);
+			fscanf(fp4,"%s%d",mnemonic,&code);
+			while(strcmp(opcode,mnemonic)!=0)
+			fscanf(fp4,"%s%d",mnemonic,&code);
+			if(strcmp(operand,"**")==0)
+			{
+				fprintf(fp1,"%d\t%s\t%s\t%s\t%d0000\n",locctr,label,opcode,operand,code);
+			} 
+			else
+			{
+				rewind(fp2);
+					fscanf(fp2,"%s%d",symbol,&add);
+					while(strcmp(operand,symbol)!=0)
+					{ 
+						fscanf(fp2,"%s%d",symbol,&add);
+					}
+					fprintf(fp1,"%d\t%s\t%s\t%s\t%d%d\n",locctr,label,opcode,operand,code,add);
+			}
+		} 
+		fscanf(fp3,"%d%s%s%s\n",&locctr,label,opcode,operand);
+	}
+	fprintf(fp1,"%d\t%s\t%s\t%s\n",locctr,label,opcode,operand);
+	printf("FINISHED\n\n");
+	fclose(fp1);
+	fclose(fp2);
+	fclose(fp3);
+	fclose(fp4);
+}
